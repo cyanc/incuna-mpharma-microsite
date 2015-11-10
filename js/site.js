44f7f5b353dc9f7b0ec369dec7cf78e5d41a12f2
Cyan Collier <cyan@incuna.com>
2015-11-10 16:28:41 +0000

    added as new

---
+++ js/site.js
@@ -0,0 +1,24 @@
+// Website Scripts
+
+$(document).ready(function(){
+
+<!----  appstore button - google play button hover ---->
+$(".social_icons_wrapper a").fadeTo("fast", 1.0); 
+$(".social_icons_wrapper a").hover(function(){
+$(this).fadeTo("fast", 0.6); 
+},function(){
+$(this).fadeTo("fast", 1.0); 
+});
+
+});
+
+
+<!----  fadein header ---->
+$(window).bind("load", function() {
+
+
+});
+
+
+
+
